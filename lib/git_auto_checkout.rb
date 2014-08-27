506f286f95b127a405d780cc10e90dc9f825f309
Saghm Rossi <saghmrossi@gmail.com>
2014-08-27 18:27:51 -0400

    exits if run in non-git directory

--- lib/git_auto_checkout.rb
+++ lib/git_auto_checkout.rb
@@ -6,6 +6,11 @@ module GitAutoCheckout
   module_function
 
   def run
+    if `git rev-parse --is-inside-work-tree 2>&1`.split.first == "fatal:"
+      $stderr.puts "Error: current directory is not a git repository"
+      exit 1
+    end
+
     git_checkout_helper = GitCheckoutHelper.new
     git_checkout_helper.make_commit
   end
